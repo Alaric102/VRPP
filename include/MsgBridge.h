18a5e4071004983751083e12538b6449029f292b
ildar <ildaralaric1998@gmail.com>
2022-03-06 18:39:14 +0300

    Templates added

--- include/MsgBridge.h
+++ include/MsgBridge.h
@@ -6,7 +6,8 @@
 #include <iostream>
 
 enum UnityCommands{
-    setStartPoint = 1
+    setStartPoint = 1,
+    setGoalPoint
 };
 
 struct Vector2int_t{
@@ -15,6 +16,13 @@ struct Vector2int_t{
 };
 
 static Vector2int_t CmdSetStartPoint(std::stringstream &ss){
+    Vector2int_t res = {0, 0};
+    ss >> res.x;
+    ss >> res.y;
+    return res;
+}
+
+static Vector2int_t CmdSetGoalPoint(std::stringstream &ss){
     Vector2int_t res = {0, 0};
     ss >> res.x;
     ss >> res.y;
@@ -31,8 +39,13 @@ void processString(std::string s){
         case setStartPoint: {
             Vector2int_t start2D = CmdSetStartPoint(sStream);
             std::cout << start2D.x << " " << start2D.y << std::endl;
-        }
             break;
+        }
+        case setGoalPoint: {
+            Vector2int_t goal2D = CmdSetStartPoint(sStream);
+            std::cout << goal2D.x << " " << goal2D.y << std::endl;
+            break;
+        }
         default:
             break;
     }
---
+++ include/Planner.h
@@ -0,0 +1,25 @@
+#ifndef _PLANNER_H_
+#define _PLANNER_H_
+
+#include <ros/ros.h>
+#include <geometry_msgs/Pose2D.h>
+
+class Planner
+{
+private:
+    /* data */
+public:
+    Planner(/* args */);
+    ~Planner();
+};
+
+Planner::Planner(/* args */)
+{
+}
+
+Planner::~Planner()
+{
+}
+
+
+#endif
